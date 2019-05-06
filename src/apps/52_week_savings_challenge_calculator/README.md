03692d52bc72c30f0041a6b35b92cefb7c5416b0
domiebett <dbett49@gmail.com>
2019-05-06 18:01:01 +0300

    adds the 52 week savings challenge calc

---
+++ src/apps/52_week_savings_challenge_calculator/README.md
@@ -0,0 +1,5 @@
+# 52 WEEK SAVINGS CHALLENGE CALCULATOR
+
+This is a simple node app designed to help me calculate how much I need to save every week to achieve my goals with the Mshwari 52 week savings challenge. If you know what this challenge is, it is designed to help individuals save weekly with increments of 100 shillings (in my case).
+
+This app can be run on the terminal by running node app.js while inside the project folder. It is not a simple application so changes to dates that one needs to view must be done within the application.
---
+++ src/apps/52_week_savings_challenge_calculator/app.js
@@ -0,0 +1,52 @@
+const YEAR = 2019;
+const oneWeekMs = 1000 * 60 * 60 * 24 * 7;
+const firstJanuary = new Date(YEAR, 0, 1);
+const firstJanuaryMs = firstJanuary.getTime();
+
+function printSavingsChart(lastDay = new Date(), totalAmountSaved = 0) {
+    const lastDayMs = lastDay.getTime();
+
+    const intervalOutput = getIntervalOutput(firstJanuaryMs, lastDayMs);
+
+    printIntervalOutput(intervalOutput);
+}
+
+function getIntervalOutput(firstJanuaryMs, lastDayMs) {
+    const todayMsDifference = lastDayMs - firstJanuaryMs;
+    const numberOfWeeks = todayMsDifference / oneWeekMs;
+
+    return getWeeklyIntervalOutput(numberOfWeeks);
+}
+
+function getWeeklyIntervalOutput(numberOfWeeks, totalSavedAmount = 0, increment = 100) {
+
+    let weeklyIntervalOutput = {};
+
+    for (let i = 1; i <= numberOfWeeks; i++) {
+        totalSavedAmount += increment;
+        increment += 100;
+
+        let firstWeekDayDate = new Date(((i * oneWeekMs) + firstJanuaryMs) - oneWeekMs).toDateString();
+        let lastWeekDayDate = new Date((i * oneWeekMs) + firstJanuaryMs).toDateString();
+
+        weeklyIntervalOutput[i] = {
+            totalSavedAmount: totalSavedAmount, nextAmount: increment,
+            firstDay: firstWeekDayDate, lastDay: lastWeekDayDate
+        };
+    }
+
+    return weeklyIntervalOutput;
+}
+
+function printIntervalOutput(intervalOutput) {
+    for(let key in intervalOutput) {
+        console.log(`Week: ${key} (${intervalOutput[key].firstDay} - ${intervalOutput[key].lastDay})`,
+                    '\n--------------------------------------------\n',
+                    `Savings: ${intervalOutput[key].totalSavedAmount} \t`,
+                    `Next Deposit: ${intervalOutput[key].nextAmount}\n`);
+    }
+}
+
+const LASTDAYMONTH = 6;
+
+printSavingsChart(new Date(YEAR, (LASTDAYMONTH - 1), 31));
