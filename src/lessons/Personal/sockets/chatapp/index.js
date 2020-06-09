ba5e84da0af575560550d6ed316e67420c162dd7
domiebett <dbett49@gmail.com>
2020-06-09 14:21:32 +0300

    move socket functionality

--- src/lessons/Personal/sockets/chatapp/index.js
+++ src/lessons/Personal/sockets/chatapp/index.js
@@ -1,21 +1,6 @@
 const app = require('./app');
+const socket = require('./lib/socket');
 
 const server = app.listen(3000, () => console.log('App is listening on port 3000'));
 
-const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-    console.log('New user connected');
-
-    socket.username = 'Anonymous';
-    socket.on('change_username', (data) => {
-        console.log('username changed to ' + data.username);
-        const isChange = socket.username !== 'Anonymous' ? true : false;
-        socket.username = data.username;
-        io.sockets.emit('username_changed', { username: socket.username, isChange });
-    });
-
-    socket.on('new_message', (data) => {
-        console.log('new message sent', data.newMessage);
-        io.sockets.emit('new_message', { message: data.message, username: socket.username });
-    });
-});
+socket.setUp(server);
